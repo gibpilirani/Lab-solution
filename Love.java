06d28fbcaf40db47c9cc9a0e7611c096f2374719
gibpilirani <105055832+gibpilirani@users.noreply.github.com>
2023-08-13 12:34:56 +0200

    Add files via upload

---
+++ Love.java
@@ -0,0 +1,8 @@
+public class Love {
+    public static void main(String[] args) {
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+    }
+    
+}
---
+++ Pattern.java
@@ -0,0 +1,10 @@
+public class Pattern{
+    public static void main(String[] args) {
+        System.out.println("\t" +" " + " " + "J");
+        System.out.println("J" + "\t" + "aaaa" + "\t" + "V" + "\t" + "\t" + " vaaa");
+        System.out.println("J" + "\t" + " J" + "\t" + "aa" + "\t\t" + "v" + " " + "v"  + "\t\t" + "a " + " a " );
+        System.out.println("J "  + " \t " + " aaaa " +  "\t\t" + " V" + "\t\t" + "aaaa");
+    }
+} 
+
+
